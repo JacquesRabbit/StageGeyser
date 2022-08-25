d8155245bae2a21408ff8d649583510ece0f4f4f
JacquesRabbit <69579262+JacquesRabbit@users.noreply.github.com>
2022-08-25 15:53:36 +1000

    Tried to fix an issue

--- renderer.js
+++ renderer.js
@@ -2,15 +2,9 @@ const contentElement = document.getElementById('content')
 const fileOpen = "";
 
 window.electronAPI.openFile((_event, value) => {
-    contentElement.innerHTML = value
-    updateWindow
+
 })
-function updateWindow() {
-    if(fileOpen = "") {
-        contentElement.innerHTML = "<button onclick='closeWindow()'>Open a File</button>"
-    }
-}
+
 function closeWindow() {
-    contentElement.innerHTML = ""
-    updateWindow
+    
 }
