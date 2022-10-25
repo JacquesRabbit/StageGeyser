0a2392470a65c7ffcef5cccf46362b94c73505eb
JacquesRabbit <jsrabbidge@gmail.com>
2022-10-25 17:43:41 +1000

    Reset

--- StageGeyser/Views/MainWindow.axaml.cs
+++ StageGeyser/Views/MainWindow.axaml.cs
@@ -1,16 +1,12 @@
 using Avalonia.Controls;
-using System;
-namespace StageGeyser
+
+namespace StageGeyser.Views
 {
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = this;
-        }
-        private void OpenFile(){
-            Console.WriteLine("Hello, World!");
         }
     }
 }
